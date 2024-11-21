d2e7d3a576032c880e45e1780f9324568ecbabe0
permalik <permalik@protonmail.com>
2024-11-20 19:48:00 -0600

    feat: init allocator, file read

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,32 @@
 const std = @import("std");
 
-fn main() void {
+pub fn main() void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer {
-        gpa.deinit();
+        _ = gpa.deinit();
     }
 
-    const bytes = try allocator.alloc(u8, 8);
-    defer allocator.free(bytes);
+    // const bytes = try allocator.alloc(u8, 8);
+    // defer allocator.free(bytes);
+
+    const f = try std.fs.openFileAbsolute("/Users/tymalik/Documents/git/markdown_parser_zig/src/test.md", .{ .mode = .read_only });
+    defer f.close();
+
+    var buffered = std.io.bufferedReader(f.reader());
+    var reader = buffered.reader();
+
+    var arr = std.ArrayList(u8).init(allocator);
+    defer arr.deinit();
+
+    var byte_count: usize = 0;
+    while (true) {
+        reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+        byte_count += arr.items.len;
+        arr.clearRetainingCapacity();
+    }
+    std.debug.print("{d} bytes", .{byte_count});
 }
---
+++ src/test.md
@@ -0,0 +1,76 @@
+# Heading Level 1
+## Heading Level 2
+### Heading Level 3
+#### Heading Level 4
+##### Heading Level 5
+###### Heading Level 6
+
+---
+***
+___
+
+## Emphasis
+
+This is *italic*, and this is **bold**. You can also combine them: ***bold and italic***.
+
+## Blockquotes
+
+> This is a blockquote.
+> It can span multiple lines.
+
+## Lists
+
+### Unordered List
+
+- Item 1
+- Item 2
+  - Subitem 1
+  - Subitem 2
+- Item 3
+
+### Ordered List
+
+1. First item
+2. Second item
+   1. Subitem 1
+   2. Subitem 2
+3. Third item
+
+### Task List
+
+- [ ] Task one
+- [ ] Task two
+- [ ] Task three
+
+## Line Breaks
+
+  
+
+First Break  
+
+Second Break  
+
+## Single Space Line
+
+ 
+
+## Links
+
+This is a [link to OpenAI](https://www.openai.com).
+
+## Images
+
+![OpenAI Logo](https://openai.com/favicon.ico)
+
+## Code
+
+Inline code: `console.log("Hello, world!");`
+
+### Code Block
+
+```javascript
+function greet() {
+    console.log("Hello, world!");
+}
+greet();
+```
